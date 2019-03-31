12ceaa1e277429d42a0d7203ae0e585c9f095ecc
Courtney Hasenstab <hasenstab.courtney@gmail.com>
2019-03-31 13:09:10 -0500

    updated search.html for better results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static java.text.MessageFormat.*;
+import static org.launchcode.controllers.ListController.*;
 
 /**
  * Created by LaunchCode
@@ -17,15 +21,17 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
-    public String search(Model model) {
+    @RequestMapping ( value = "" )
+    public
+    String search (Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results", method = RequestMethod.GET)
-    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    @RequestMapping ( value = "results", method = RequestMethod.GET )
+    public
+    String search (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
 
         if (searchType.equals("all")) {
@@ -34,7 +40,13 @@ public class SearchController {
             model.addAttribute("columns", ListController.columnChoices);
             int numResult = jobs.size();
             String results = numResult + " Result(s)";
-            model.addAttribute("results", results).addAttribute("jobs", jobs);
+            model.addAttribute("results", results);
+            if (!searchTerm.equals("")) {
+                model.addAttribute("", MessageFormat.format("{0}", searchTerm));
+            } else {
+                model.addAttribute("", "All Jobs");
+            }
+            model.addAttribute("jobs", jobs);
             return "search";
         } else {
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
@@ -43,14 +55,12 @@ public class SearchController {
             String results = numResult + " Result(s)";
             model.addAttribute("results", results);
             if (!searchTerm.equals("")) {
-                model.addAttribute("title", "All " + ListController.columnChoices.get(searchType) + " Values containing keyword(s) " + searchTerm);
+                model.addAttribute("title", MessageFormat.format("", ListController.columnChoices.get(searchType), searchTerm));
             } else {
-                model.addAttribute("title", "All " + ListController.columnChoices.get(searchType) + " Values");
+                model.addAttribute("title", " " + ListController.columnChoices.get(searchType) + "");
             }
             model.addAttribute("jobs", jobs);
             return "search";
         }
     }
 }
-
-
